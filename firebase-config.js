50d9edf94e9672bf060488838023c6e0699a242f
mohdsah <mohdsah5@gmail.com>
2025-05-09 09:12:53 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCGFV1ThnWNm5pMMeOycnoEenu0WdmxStM",
@@ -15,5 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { auth, firebaseConfig };
+export { auth, db };
